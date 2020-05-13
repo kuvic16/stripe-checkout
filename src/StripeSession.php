03a8cbba6705be7a890af91eef7af21986dee59c
Shaiful Islam <kuvic16@gmail.com>
2020-05-13 16:40:58 +0600

    Separated the stripe session class

---
+++ src/StripeSession.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace StripePhp\App;
+
+use GuzzleHttp\Client;
+
+/**
+ * Stripe Session Class
+ */
+final class StripeSession{
+
+    /**
+     * GuzzleHttp Client
+     * 
+     * @var GuzzleHttp | object
+     */
+    private $client;
+
+    /**
+     * User plan
+     * 
+     * @var String | 'BASIC'
+     */
+    private $plan;
+
+    /**
+     * Initialize the class
+     */
+    public function __construct(){
+        $this->client = new Client(['base_uri' => $_ENV['STRIPE_BASE_URL']]);
+        $this->plan   = isset($_GET['plan']) ? strtoupper($_GET['plan']) : $_ENV['DEFAULT_PLAN']; 
+    }
+
+    /**
+     * Create stripe session
+     * 
+     * @return json
+     */
+    public function create(){
+        $output = [];
+        try{
+            $headers = [
+                'Authorization' => 'Bearer ' . $_ENV['STRIPE_KEY'],        
+                'Accept'        => 'application/x-www-form-urlencoded',
+            ];
+            $data = [
+                'success_url'          => $_ENV['SUCCESS_URL'],
+                'cancel_url'           => $_ENV['CANCEL_URL'],
+                'payment_method_types' => ['card'],
+                'line_items' => [
+                    [
+                        'name'        => $_ENV["{$this->plan}.NAME"],
+                        'description' => $_ENV["{$this->plan}.DESCRIPTION"],
+                        'amount'      => floatval($_ENV["{$this->plan}.PRICE"]) * 100,
+                        'currency'    => 'usd',
+                        'quantity'    => 1,
+                    ],
+                ],
+            ];
+
+            $response = $this->client->request('POST', $_ENV['STRIPE_SESSION_URL'],
+                ['form_params' => $data, 'headers' => $headers]
+            );
+            $obj = json_decode($response->getBody());
+            $output = [
+                'success' => 1,
+                'id' => $obj->id
+            ];
+        }catch(\Exception $ex){
+            $output = [
+                'success' => 0,
+                'message' => $ex->getMessage()
+            ];
+        }finally{
+            header('Content-type: application/json');
+            echo json_encode( $output );
+        }
+    }
+}
--- stripe-session.php
+++ stripe-session.php
@@ -2,89 +2,15 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use GuzzleHttp\Client;
 use Dotenv\Dotenv;
 
-/**
- * Stripe Session Class
- */
-final class StripeSession{
-
-    /**
-     * GuzzleHttp Client
-     * 
-     * @var GuzzleHttp | object
-     */
-    private $client;
-
-    /**
-     * User plan
-     * 
-     * @var String | 'BASIC'
-     */
-    private $plan;
-
-    /**
-     * Initialize the class
-     */
-    public function __construct(){
-        Dotenv::createImmutable(__DIR__)->load();
-        $this->client = new Client(['base_uri' => $_ENV['STRIPE_BASE_URL']]);
-        $this->plan   = isset($_GET['plan']) ? strtoupper($_GET['plan']) : $_ENV['DEFAULT_PLAN']; 
-    }
-
-    /**
-     * Create stripe session
-     * 
-     * @return json
-     */
-    public function create(){
-        $output = [];
-        try{
-            $headers = [
-                'Authorization' => 'Bearer ' . $_ENV['STRIPE_KEY'],        
-                'Accept'        => 'application/x-www-form-urlencoded',
-            ];
-            $data = [
-                'success_url'          => $_ENV['SUCCESS_URL'],
-                'cancel_url'           => $_ENV['CANCEL_URL'],
-                'payment_method_types' => ['card'],
-                'line_items' => [
-                    [
-                        'name'        => $_ENV["{$this->plan}.NAME"],
-                        'description' => $_ENV["{$this->plan}.DESCRIPTION"],
-                        'amount'      => floatval($_ENV["{$this->plan}.PRICE"]) * 100,
-                        'currency'    => 'usd',
-                        'quantity'    => 1,
-                    ],
-                ],
-            ];
-
-            $response = $this->client->request('POST', $_ENV['STRIPE_SESSION_URL'],
-                ['form_params' => $data, 'headers' => $headers]
-            );
-            $obj = json_decode($response->getBody());
-            $output = [
-                'success' => 1,
-                'id' => $obj->id
-            ];
-        }catch(\Exception $ex){
-            $output = [
-                'success' => 0,
-                'message' => $ex->getMessage()
-            ];
-        }finally{
-            header('Content-type: application/json');
-            echo json_encode( $output );
-        }
-    }
-}
 
 /**
  * Initialize the main class
  */
 function stripe_session(){
-    (new StripeSession())->create();
+    Dotenv::createImmutable(__DIR__)->load();        
+    (new StripePhp\App\StripeSession())->create();
 }
 
 // kick-off the class
